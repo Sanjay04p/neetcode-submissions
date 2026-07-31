9abd51efe4bcd25d6be81ab7c4f94891975c370f
Sanjay Pattanashetti <sanjayp0412005@gmail.com>
2026-08-01 01:40:01 +0530

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    #brute force
+    def singleNumber(self, nums: List[int]) -> int:
+        c={}
+        for i in nums:
+            if i in c:
+                c[i]+=1
+            else:
+                c[i]=1
+        res={k:v for k,v in c.items() if v==1}
+        return list(res.keys())[0]
